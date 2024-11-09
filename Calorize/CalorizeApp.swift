85caed3cf31d4578012f302691a8bf2e714afc4a
Jacob Lee <jacob.lee.jiege@sji.edu.sg>
2024-11-09 11:09:11 +0800

    Initial Commit

---
+++ Calorize/CalorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorizeApp.swift
+//  Calorize
+//
+//  Created by J Family on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CalorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calorize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calorize
+//
+//  Created by J Family on 9/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
