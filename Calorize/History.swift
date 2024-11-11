19d39aea670f52c850f6a10b4014af016b02ec6e
timatolau <168737894+timatolau@users.noreply.github.com>
2024-11-11 12:28:30 +0800

    added NavigationLink for the food item details in History

--- Calorize/History.swift
+++ Calorize/History.swift
@@ -7,21 +7,28 @@ struct HistoryView: View {
     var body: some View{
         @Bindable var foodItemManager = foodItemManager
         NavigationStack {
-            List(foodItemManager.foodItems) { FoodItem in
-                HStack{
-                    VStack {
-                        Text(FoodItem.name)
-                         
-                        Text(FoodItem.date, style: .date)
+            List($foodItemManager.foodItems) { $FoodItem in
+                NavigationLink{
+                    LogDetailView(item: $FoodItem)
+                }label:{
+                    HStack{
+                        VStack(alignment: .leading) {
+                            Text(FoodItem.name)
+                            HStack{
+                                Text(FoodItem.date, style: .date)
+                                Text(FoodItem.date, style: .time)
+                            }
                             .font(.subheadline)
                             .opacity(0.5)
+                        }
+                        Spacer()
+                        Text("\(FoodItem.calories) Cal")
+                        
                     }
-                    Spacer()
-                    Text("\(FoodItem.calories) Cal")
-                    
                 }
                
-              
+                
+                
                 
             }
             
